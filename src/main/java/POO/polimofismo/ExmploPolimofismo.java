7a427893c3c8c9aa1d7e93f02c223729f6e52d6d
jbwinchesterjw <49697170+jbwinchesterjw@users.noreply.github.com>
2022-09-25 10:01:05 -0300

    Media vetor

---
+++ src/main/java/POO/polimofismo/ExmploPolimofismo.java
@@ -0,0 +1,12 @@
+package POO.polimofismo;
+
+public class ExmploPolimofismo {
+    /**
+     * Polimorfismo significa "muitas formas", é o termo definido em linguagens orientadas a objeto, como por exemplo Java, C#, que permite ao desenvolvedor usar o mesmo elemento de formas diferentes.
+     * Polimorfismo denota uma situação na qual um objeto pode se comportar de maneiras diferentes ao receber uma mensagem. No Polimorfismo temos dois tipos:
+     *   Polimorfismo Estático ou Sobrecarga.
+     *   Polimorfismo Dinâmico ou Sobreposição.
+     *
+     *   O Polimorfismo Estático se dá quando temos a mesma operação implementada várias vezes na mesma classe
+     */
+}
